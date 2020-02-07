c72023e124897dd318c1e7193814d632a04468e1
Cristal Alatorre Perez <cristalcodes@gmail.com>
2020-02-07 15:10:59 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 class Formatter {
   //add static methods here
+  static capitalize(string){
+    return string.charAt(0).toUpperCase()+ string.slice(1)
+  }
+
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9-' ]+/g, '');
+  }
+
+  static titleize(string){
+    let capitalizedString=[]
+    let wordArray = string.split(' ')
+    let prohibited = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+
+  for (let i = 0; i<wordArray.length; i++){
+      if (prohibited.includes(wordArray[i]) && wordArray[i] != wordArray[0]) {
+        capitalizedString.push(wordArray[i])
+      }
+      else {
+        let word = wordArray[i].charAt(0).toUpperCase() + wordArray[i].slice(1);
+        capitalizedString.push(word)
+      }
+  }
+
+  return capitalizedString.join(' ');
+}
 }
